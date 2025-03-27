b65219e17ec1f48b5a07e8f0103482a9b4c466cc
mihai-bontea <mihaibontea@protonmail.com>
2025-03-27 16:10:08 +0200

    floyd warshall in separate file

--- sol3/Data.h
+++ sol3/Data.h
@@ -8,24 +8,10 @@
 #include <iostream>
 #include <limits>
 
-#include "BS_thread_pool.hpp"
+#include "ParallelFloydWarshall.h"
 
 #define MAX_VERTICES 11348
-//#define MAX_VERTICES 11
 #define MAX_CARS 8
-#define NO_PATH -1
-constexpr int INF = std::numeric_limits<int>::max();
-
-struct Edge
-{
-    int neighbor, cost, length;
-
-    Edge(int neighbor, int cost, int length)
-            : neighbor(neighbor)
-            , cost(cost)
-            , length(length)
-    {}
-};
 
 struct Data
 {
@@ -33,7 +19,6 @@ struct Data
     static std::array<std::vector<Edge>, MAX_VERTICES> adjacency;
     static std::array<std::array<int, MAX_VERTICES>, MAX_VERTICES> shortest_dist;
     static std::array<std::array<int, MAX_VERTICES>, MAX_VERTICES> next_vertex;
-    static std::string floyd_warshall_filename;
 
     explicit Data(const std::string& filename)
     {
@@ -62,7 +47,8 @@ struct Data
             if (type == 2)
                 adjacency[node_b].emplace_back(node_a, cost, length);
         }
-        floyd_warshall();
+
+        ParallelFloydWarshall floyd_warshall(shortest_dist, next_vertex, adjacency);
     }
 
     static void write_to_file(const std::string& filename, const std::vector<std::vector<int>>& car_paths)
@@ -79,122 +65,8 @@ struct Data
                 fout << junction << '\n';
         }
     }
-private:
-    void floyd_warshall()
-    {
-        std::ifstream fin(floyd_warshall_filename);
-        if (fin)
-        {
-            std::cout << "Shortest distances already computed, reading from file...\n";
-            read_precomputed_distances_from_file(fin);
-            return;
-        }
-
-        // Initializing the distance and next_vertex matrices
-        for (int vertex_id = 0; vertex_id < nr_junctions; ++vertex_id)
-        {
-            std::fill(shortest_dist[vertex_id].begin(), shortest_dist[vertex_id].end(), INF);
-            std::fill(next_vertex[vertex_id].begin(), next_vertex[vertex_id].end(), NO_PATH);
-
-            shortest_dist[vertex_id][vertex_id] = 0;
-            next_vertex[vertex_id][vertex_id] = vertex_id;
-
-            for (const auto& edge : adjacency[vertex_id])
-            {
-                shortest_dist[vertex_id][edge.neighbor] = edge.cost;
-                next_vertex[vertex_id][edge.neighbor] = edge.neighbor;
-            }
-        }
-//        const int nr_threads = 11;
-//        int chunk_size = nr_junctions / nr_threads;
-//        BS::thread_pool pool(nr_threads);
-//        for (int k = 0; k < nr_junctions; ++k)
-//        {
-//            std::vector<std::future<void>> worker_futures;
-//
-//            for (int t = 0; t < nr_threads; ++t)
-//            {
-//                int start = t * chunk_size;
-//                int end = (t == nr_threads - 1)? nr_junctions : (t + 1) * chunk_size;
-//                worker_futures.push_back(pool.submit_task([&](){return floyd_warshall_worker(start, end, k);}));
-//                std::cout << "Making future for interval [" << start << ", " << end << "]\n";
-//            }
-//            for (auto& future : worker_futures)
-//                future.get();
-//        }
-
-        // main floyd-warshall
-        for (int k = 0; k < nr_junctions; ++k)
-        {
-            for (int i = 0; i < nr_junctions; ++i)
-            {
-                for (int j = 0; j < nr_junctions; ++j)
-                {
-                    if (shortest_dist[i][k] != INF && shortest_dist[k][j] != INF)
-                    {
-                        if (shortest_dist[i][j] > shortest_dist[i][k] + shortest_dist[k][j])
-                        {
-                            shortest_dist[i][j] = shortest_dist[i][k] + shortest_dist[k][j];
-                            next_vertex[i][j] = next_vertex[i][k];
-                        }
-                    }
-                }
-            }
-        }
-
-        std::cout << "Finished computing shortest distances, writing results to file...\n";
-        std::ofstream fout(floyd_warshall_filename);
-        write_distances_to_file(fout);
-    }
-
-    void floyd_warshall_worker(int start, int end, int k)
-    {
-        for (int i = start; i < end; i++)
-        {
-            for (int j = 0; j < nr_junctions; j++)
-            {
-                if (shortest_dist[i][k] != INF && shortest_dist[k][j] != INF)
-                {
-                    if (shortest_dist[i][j] > shortest_dist[i][k] + shortest_dist[k][j])
-                    {
-                        shortest_dist[i][j] = shortest_dist[i][k] + shortest_dist[k][j];
-                        next_vertex[i][j] = next_vertex[i][k];
-                    }
-                }
-            }
-        }
-    }
-
-    void read_precomputed_distances_from_file(std::ifstream& fin)
-    {
-        for (int i = 0; i < nr_junctions; ++i)
-            for (int j = 0; j < nr_junctions; ++j)
-                fin >> shortest_dist[i][j];
-
-        for (int i = 0; i < nr_junctions; ++i)
-            for (int j = 0; j < nr_junctions; ++j)
-                fin >> next_vertex[i][j];
-    }
-
-    void write_distances_to_file(std::ofstream& fout)
-    {
-        for (int i = 0; i < nr_junctions; ++i)
-        {
-            for (int j = 0; j < nr_junctions; ++j)
-                fout << shortest_dist[i][j] << " ";
-            fout << '\n';
-        }
-        fout << '\n';
-        for (int i = 0; i < nr_junctions; ++i)
-        {
-            for (int j = 0; j < nr_junctions; ++j)
-                fout << next_vertex[i][j] << " ";
-            fout << '\n';
-        }
-    }
 };
 
 std::array<std::vector<Edge>, MAX_VERTICES> Data::adjacency;
 std::array<std::array<int, MAX_VERTICES>, MAX_VERTICES> Data::shortest_dist;
 std::array<std::array<int, MAX_VERTICES>, MAX_VERTICES> Data::next_vertex;
-std::string Data::floyd_warshall_filename = "../shortest_distances.txt";
---
+++ sol3/ParallelFloydWarshall.h
@@ -0,0 +1,157 @@
+#pragma once
+
+#include <fstream>
+
+#include "BS_thread_pool.hpp"
+//#include "Data.h"
+
+#define MAX_VERTICES 11348
+#define NO_PATH -1
+constexpr int INF = std::numeric_limits<int>::max();
+
+struct Edge
+{
+    int neighbor, cost, length;
+
+    Edge(int neighbor, int cost, int length)
+            : neighbor(neighbor)
+            , cost(cost)
+            , length(length)
+    {}
+};
+
+using Matrix = std::array<std::array<int, MAX_VERTICES>, MAX_VERTICES>;
+using AdjacencyList = std::array<std::vector<Edge>, MAX_VERTICES>;
+
+class ParallelFloydWarshall
+{
+public:
+    ParallelFloydWarshall(Matrix& shortest_dist, Matrix& next_vertex, const AdjacencyList& adjacency)
+    : shortest_dist(shortest_dist)
+    , next_vertex(next_vertex)
+    , adjacency(adjacency)
+    {
+        floyd_warshall();
+    }
+private:
+
+    void floyd_warshall()
+    {
+        std::ifstream fin(floyd_warshall_filename);
+        if (fin)
+        {
+            std::cout << "Shortest distances already computed, reading from file...\n";
+            read_precomputed_distances_from_file(fin);
+            return;
+        }
+
+        // Initializing the distance and next_vertex matrices
+        for (int vertex_id = 0; vertex_id < MAX_VERTICES; ++vertex_id)
+        {
+            std::fill(shortest_dist[vertex_id].begin(), shortest_dist[vertex_id].end(), INF);
+            std::fill(next_vertex[vertex_id].begin(), next_vertex[vertex_id].end(), NO_PATH);
+
+            shortest_dist[vertex_id][vertex_id] = 0;
+            next_vertex[vertex_id][vertex_id] = vertex_id;
+
+            for (const auto& edge : adjacency[vertex_id])
+            {
+                shortest_dist[vertex_id][edge.neighbor] = edge.cost;
+                next_vertex[vertex_id][edge.neighbor] = edge.neighbor;
+            }
+        }
+//        const int nr_threads = 11;
+//        int chunk_size = nr_junctions / nr_threads;
+//        BS::thread_pool pool(nr_threads);
+//        for (int k = 0; k < nr_junctions; ++k)
+//        {
+//            std::vector<std::future<void>> worker_futures;
+//
+//            for (int t = 0; t < nr_threads; ++t)
+//            {
+//                int start = t * chunk_size;
+//                int end = (t == nr_threads - 1)? nr_junctions : (t + 1) * chunk_size;
+//                worker_futures.push_back(pool.submit_task([&](){return floyd_warshall_worker(start, end, k);}));
+//                std::cout << "Making future for interval [" << start << ", " << end << "]\n";
+//            }
+//            for (auto& future : worker_futures)
+//                future.get();
+//        }
+
+        // main floyd-warshall
+        for (int k = 0; k < MAX_VERTICES; ++k)
+        {
+            for (int i = 0; i < MAX_VERTICES; ++i)
+            {
+                for (int j = 0; j < MAX_VERTICES; ++j)
+                {
+                    if (shortest_dist[i][k] != INF && shortest_dist[k][j] != INF)
+                    {
+                        if (shortest_dist[i][j] > shortest_dist[i][k] + shortest_dist[k][j])
+                        {
+                            shortest_dist[i][j] = shortest_dist[i][k] + shortest_dist[k][j];
+                            next_vertex[i][j] = next_vertex[i][k];
+                        }
+                    }
+                }
+            }
+        }
+
+        std::cout << "Finished computing shortest distances, writing results to file...\n";
+        std::ofstream fout(floyd_warshall_filename);
+        write_distances_to_file(fout);
+    }
+
+    Matrix& shortest_dist;
+    Matrix& next_vertex;
+    const AdjacencyList& adjacency;
+    static std::string floyd_warshall_filename;
+
+    void floyd_warshall_worker(int start, int end, int k)
+    {
+        for (int i = start; i < end; i++)
+        {
+            for (int j = 0; j < MAX_VERTICES; j++)
+            {
+                if (shortest_dist[i][k] != INF && shortest_dist[k][j] != INF)
+                {
+                    if (shortest_dist[i][j] > shortest_dist[i][k] + shortest_dist[k][j])
+                    {
+                        shortest_dist[i][j] = shortest_dist[i][k] + shortest_dist[k][j];
+                        next_vertex[i][j] = next_vertex[i][k];
+                    }
+                }
+            }
+        }
+    }
+
+    void read_precomputed_distances_from_file(std::ifstream& fin)
+    {
+        for (int i = 0; i < MAX_VERTICES; ++i)
+            for (int j = 0; j < MAX_VERTICES; ++j)
+                fin >> shortest_dist[i][j];
+
+        for (int i = 0; i < MAX_VERTICES; ++i)
+            for (int j = 0; j < MAX_VERTICES; ++j)
+                fin >> next_vertex[i][j];
+    }
+
+    void write_distances_to_file(std::ofstream& fout)
+    {
+        for (int i = 0; i < MAX_VERTICES; ++i)
+        {
+            for (int j = 0; j < MAX_VERTICES; ++j)
+                fout << shortest_dist[i][j] << " ";
+            fout << '\n';
+        }
+        fout << '\n';
+        for (int i = 0; i < MAX_VERTICES; ++i)
+        {
+            for (int j = 0; j < MAX_VERTICES; ++j)
+                fout << next_vertex[i][j] << " ";
+            fout << '\n';
+        }
+    }
+};
+
+std::string ParallelFloydWarshall::floyd_warshall_filename = "../shortest_distances.txt";
